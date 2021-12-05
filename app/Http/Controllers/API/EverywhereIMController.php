f3be863492b43b50ea6e6f5144ca7ef12d85990d
ahazna <contact@ahazna.com>
2021-12-05 15:17:24 +0100

    API everywhereIM and ColorController created

---
+++ app/Http/Controllers/API/EverywhereIMController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\ColorController;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Color;
+use App\Models\Ccode;
+use \Illuminate\Support\Facades\URL;
+
+class EverywhereIMController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = User::factory()->create();
+        $color = Color::inRandomOrder()->first();
+        $user->colors()->attach($color);
+
+        return redirect('api/color');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,6 @@ class User extends Authenticatable
      */
 
     public function colors(){
-        return $this->belongsToMany(Color::class);
+        return $this->belongsToMany(Color::class)->withTimestamps();
     }
 }
